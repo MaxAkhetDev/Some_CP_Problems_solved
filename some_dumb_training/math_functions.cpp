622ede159bec18361feccee7b971cce7cc9d816f
MaxAkhetDev <franoisdassisemaxwell.bohm@usmba.ac.ma>
2025-10-01 23:00:11 +0100

    Auto-commit: 2025-10-01 23:00:11

--- some_dumb_training/math_functions.cpp
+++ some_dumb_training/math_functions.cpp
@@ -4,15 +4,28 @@
 using namespace std;
 
 // Returns a vector containing the first n Fibonacci numbers
-std::vector<long long> fibonacci(int n) {
-    std::vector<long long> fib;
-    if (n <= 0) return fib;
-    fib.push_back(0);
-    if (n == 1) return fib;
-    fib.push_back(1);
-    for (int i = 2; i < n; ++i) {
-        fib.push_back(fib[i-1] + fib[i-2]);
-    }
-    return fib;
+// std::vector<long long> fibonacci(int n) {
+//     std::vector<long long> fib;
+//     if (n <= 0) return fib;
+//     fib.push_back(0);
+//     if (n == 1) return fib;
+//     fib.push_back(1);
+//     for (int i = 2; i < n; ++i) {
+//         fib.push_back(fib[i-1] + fib[i-2]);
+//     }
+//     return fib;
+// }
+
+// Returns the nth number of the fibonacci pogressing
+// long long int fibonacci(int n) {
+//     if (n==0) return 0;
+//     if (n==1) return 1;
+//     return fib(n-1) + fib(n-2);
+// }
+
+void main (){
+    int x;
+    cin << x;
+    //cout>>fibonacci(x);
 }
 
